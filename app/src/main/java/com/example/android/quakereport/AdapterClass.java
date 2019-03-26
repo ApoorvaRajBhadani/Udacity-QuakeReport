bd98f6ef50016d03aabecdc806b0ee658a9cb74d
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-03-26 19:49:30 +0530

    data from json

--- app/src/main/java/com/example/android/quakereport/AdapterClass.java
+++ app/src/main/java/com/example/android/quakereport/AdapterClass.java
@@ -1,6 +1,50 @@
 package com.example.android.quakereport;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-import android.widget.AdapterView;
+import java.util.ArrayList;
 
-public class AdapterClass extends AdapterView {
+public class AdapterClass extends BaseAdapter {
+    Context context;
+    ArrayList<ModelClass> model;
+
+    public AdapterClass(Context context, ArrayList<ModelClass> model) {
+        this.context = context;
+        this.model = model;
+    }
+
+    @Override
+    public int getCount() {
+        return model.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return model.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView==null)
+            convertView = View.inflate(context,R.layout.list_layout,null);
+        TextView magnitudeTextView= (TextView)convertView.findViewById(R.id.magnitude_textview);
+        TextView locationTextView= (TextView)convertView.findViewById(R.id.location_textview);
+        TextView dateTextView= (TextView)convertView.findViewById(R.id.date_textview);
+
+        final ModelClass modelTemp = model.get(position);
+        magnitudeTextView.setText(Double.toString(modelTemp.getMagnitude()));
+        locationTextView.setText(modelTemp.getLocation());
+        dateTextView.setText(modelTemp.getDate());
+
+        return convertView;
+
+    }
 }
--- app/src/main/java/com/example/android/quakereport/ModelClass.java
+++ app/src/main/java/com/example/android/quakereport/ModelClass.java
@@ -1,4 +1,24 @@
 package com.example.android.quakereport;
 
 public class ModelClass {
+    double magnitude;
+    String Location,Date;
+
+    public ModelClass(double magnitude, String location, String date) {
+        this.magnitude = magnitude;
+        Location = location;
+        Date = date;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public String getDate() {
+        return Date;
+    }
 }
