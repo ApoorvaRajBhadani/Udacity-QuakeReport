a63f81345b68bcec20ebee7a374669733029b6b3
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-03-27 19:41:56 +0530

    string manipulated decimal formatted

--- app/src/main/java/com/example/android/quakereport/AdapterClass.java
+++ app/src/main/java/com/example/android/quakereport/AdapterClass.java
@@ -5,6 +5,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class AdapterClass extends BaseAdapter {
@@ -36,16 +37,32 @@ public class AdapterClass extends BaseAdapter {
         if(convertView==null)
             convertView = View.inflate(context,R.layout.list_layout,null);
         TextView magnitudeTextView= (TextView)convertView.findViewById(R.id.magnitude_textview);
-        TextView locationTextView= (TextView)convertView.findViewById(R.id.location_textview);
+        TextView primarylocationTextView= (TextView)convertView.findViewById(R.id.primarylocation_textview);
+        TextView locationoffsetTextView= (TextView)convertView.findViewById(R.id.locationoffset_textview);
         TextView dateTextView= (TextView)convertView.findViewById(R.id.date_textview);
         TextView timeTextView= (TextView)convertView.findViewById(R.id.time_textview);
 
-
         final ModelClass modelTemp = model.get(position);
-        magnitudeTextView.setText(Double.toString(modelTemp.getMagnitude()));
-        locationTextView.setText(modelTemp.getLocation());
+
+        String location = modelTemp.getLocation(),locationoffset,primarylocation;
+        int ofbeginindex = location.indexOf("of");
+        if(ofbeginindex>=0) {
+            locationoffset = location.substring(0, ofbeginindex+2);
+            primarylocation = location.substring(ofbeginindex+1+2,location.length());
+        }
+        else {
+            locationoffset = "Near the";
+            primarylocation = location;
+        }
+
+        DecimalFormat decimalFormat  = new DecimalFormat("0.0");
+        String formattedMagnitude = decimalFormat.format(modelTemp.getMagnitude());
+
+        magnitudeTextView.setText(formattedMagnitude);
         dateTextView.setText(modelTemp.getDate());
         timeTextView.setText(modelTemp.getTime());
+        primarylocationTextView.setText(primarylocation);
+        locationoffsetTextView.setText(locationoffset);
 
 
         return convertView;
